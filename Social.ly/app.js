314f06c4f7e2c34d02185d35456c66c4ed41d7b0
MananaSharma09 <manansharma1209@gmail.com>
2024-10-16 19:37:46 +0530

    deleted social

--- Social.ly/app.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import dotenv from "dotenv"
-
-dotenv.config({path : "./config/config.env"})
-
-const app=express();
-
-export default app;
--- Social.ly/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js"
-import connectDB from "./config/db.js"
-
-connectDB();
-
-app.listen(process.env.PORT,()=>{
-    console.log(`Sever running on port ${process.env.PORT}`)
-})
